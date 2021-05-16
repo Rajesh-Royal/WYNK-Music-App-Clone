aceca24b5780ebc883c56311c596e9ed27ed81b5
unknown <rajeshroyal896@gmail.com>
2021-05-16 11:59:25 +0530

    container style done

--- src/containers/container.tsx
+++ src/containers/container.tsx
@@ -4,10 +4,10 @@ interface Props {
     children: ReactNode
 }
 
-const container = ({children}: Props) => {
+const Container = ({children}: Props) => {
     return (
         <div className="max-w-7xl mx-auto">{children}</div>
     );
 };
 
-export default container;
+export default Container;
