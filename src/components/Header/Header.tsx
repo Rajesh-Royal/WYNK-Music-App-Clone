2560021893af30a9c81aefaaaa3b721a9c1c3045
unknown <rajeshroyal896@gmail.com>
2021-05-16 15:53:43 +0530

    CLassnames function to get classNames by conditionally

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const Header = (props: Props) => {
+    return (
+        <header>
+            This is a simple Header
+        </header>
+    );
+};
+
+export default Header;
