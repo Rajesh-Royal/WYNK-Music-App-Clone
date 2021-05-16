8b1ace94f7e26939326d06700635c0837520def0
unknown <rajeshroyal896@gmail.com>
2021-05-16 23:25:52 +0530

    Failed try to force whole app 😢😭

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState} from "react";
 import Header from "./components/Header/Header";
 
 const App = (props: any) => {
+  const [ dark, setDark ] = useState<boolean>(true);
+  const toggleTheme = () => setDark(!dark);
+  console.log(dark);
   return (
     <main>
       <Header />
