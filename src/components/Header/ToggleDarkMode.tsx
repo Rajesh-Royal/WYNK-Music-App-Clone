37300c7b2dcf582e65af0542ad2d1f5d3ba5f40f
unknown <rajeshroyal896@gmail.com>
2021-05-16 23:09:00 +0530

    theme toggle function added

---
+++ src/components/Header/ToggleDarkMode.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement, useEffect, useState } from "react";
+import NightMoon from "../../assets/icons/night.svg";
+import DaySun from "../../assets/icons/day.svg";
+
+export default function ToggleDarkMode(): ReactElement {
+  const [dark, setDark] = useState<boolean>(localStorage.WYNK_Theme === "dark");
+  if (localStorage.WYNK_Theme === "dark" || (!("WYNK_Theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+    document.documentElement.classList.add("dark");
+
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  return (
+    <>
+        {
+            dark ?
+            <img src={DaySun} className="w-5 cursor-pointer transition-all duration-500" alt="sun icon"
+            onClick={() => {
+                setDark(false);
+                localStorage.WYNK_Theme = "light";
+            }}/> :
+            <img src={NightMoon} className="w-5 cursor-pointer" alt="moon icon"
+            onClick={() => {
+                setDark(true);
+                localStorage.WYNK_Theme = "dark";
+            }}/>
+        }
+    </>
+  );
+}
