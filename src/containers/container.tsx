9180930342979a64c70ccd7b9e6c3b7a48c64d8a
unknown <rajeshroyal896@gmail.com>
2021-05-16 13:25:25 +0530

    Container story styled

--- src/containers/container.tsx
+++ src/containers/container.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from "react";
 
 export interface ContainerProps {
-    children: ReactNode
+    children: ReactNode,
+    backgroundColor?: string
 }
 
-const Container:React.FC<ContainerProps> = ({children}) => {
+const Container:React.FC<ContainerProps> = ({children, backgroundColor}) => {
+    console.log(backgroundColor);
     return (
-        <div className="max-w-7xl mx-auto">{children}</div>
+        <div className="max-w-7xl mx-auto" style={{backgroundColor}}>{children}</div>
     );
 };
 
