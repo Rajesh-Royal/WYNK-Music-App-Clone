8362647e6cfc34a5eb05841c8c3eb889df16417a
unknown <rajeshroyal896@gmail.com>
2021-05-17 10:57:47 +0530

    theme addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    'storybook-addon-themes'
   ],
   webpackFinal: async config => {
     config.module.rules = [
