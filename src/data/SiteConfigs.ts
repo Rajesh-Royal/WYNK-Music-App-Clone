22be32556bf844822a796cc19fa47dd74c0b5823
unknown <rajeshroyal896@gmail.com>
2021-05-26 21:07:43 +0530

    site config data added

---
+++ src/data/SiteConfigs.ts
@@ -0,0 +1,27 @@
+interface SiteSeoConfigsType {
+  name: string;
+  url: string;
+  icon: string;
+  AppleIcon: string;
+  title: string;
+  description: string;
+  canonical: string;
+  themeColor: string;
+  keywords: string;
+  opengraphImage: string;
+}
+
+export const SiteSeoConfigs: SiteSeoConfigsType = {
+  name: "Wynk Music - clone",
+  url: "https://wynk-clone.netlify.app/",
+  icon: "https://wynk.in/assets/favicon-wht.ico",
+  AppleIcon: "https://wynk.in/assets/icons/icon-192x192.png",
+  opengraphImage: "https://wynk.in/assets/icons/icon-192x192.png",
+  title: "Listen &amp; Download Latest MP3 Hindi, English, Bollywood Songs Online | Wynk Music",
+  description:
+    "Wynk Music - Download &amp; Listen mp3 songs, music online for free. Enjoy from over 30 Lakh Hindi, English, Bollywood, Regional, Latest, Old songs and more. Create and Listen to your playlist, like and share your favorite music on the Wynk Music app.",
+  canonical: "https://wynk-clone.netlify.app/",
+  themeColor: "#21252d",
+  keywords:
+    "Download songs, music, mp3 songs online, listen songs, MP3 songs, free online songs, wynk music, online latest songs, free music, popular songs, movie songs, listen to songs online, listen to free music, online music, free music online, latest movie songs, hindi songs, music albums, free music, online music, english songs, hollywood songs, playlists music, music online, hindi songs, play songs, online songs",
+};
