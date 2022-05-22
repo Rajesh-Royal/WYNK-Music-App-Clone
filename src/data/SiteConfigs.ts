b80263c8dd132f42c959f0b67377dcca18254fe1
Rajesh Royal <rajeshroyal896@gmail.com>
2022-05-22 20:40:07 +0530

    Update SiteConfigs.ts

--- src/data/SiteConfigs.ts
+++ src/data/SiteConfigs.ts
@@ -12,16 +12,16 @@ interface SiteSeoConfigsType {
 }
 
 export const SiteSeoConfigs: SiteSeoConfigsType = {
-  name: "Wynk Music - clone",
+  name: "Wynk Music - UI Clone",
   url: "https://wynk-clone.netlify.app/",
   icon: "https://wynk.in/assets/favicon-wht.ico",
   AppleIcon: "https://wynk.in/assets/icons/icon-192x192.png",
   opengraphImage: "https://wynk.in/assets/icons/icon-192x192.png",
-  title: "Listen &amp; Download Latest MP3 Hindi, English, Bollywood Songs Online | Wynk Music",
+  title: "Clone - Listen &amp; Download Latest MP3 Hindi, English, Bollywood Songs Online | Wynk Music",
   description:
-    "Wynk Music - Download &amp; Listen mp3 songs, music online for free. Enjoy from over 30 Lakh Hindi, English, Bollywood, Regional, Latest, Old songs and more. Create and Listen to your playlist, like and share your favorite music on the Wynk Music app.",
-  canonical: "https://wynk-clone.netlify.app/",
+    "Clone - Wynk Music - Download &amp; Listen mp3 songs, music online for free. Enjoy from over 30 Lakh Hindi, English, Bollywood, Regional, Latest, Old songs and more. Create and Listen to your playlist, like and share your favorite music on the Wynk Music app.",
+  canonical: "/",
   themeColor: "#21252d",
   keywords:
-    "Download songs, music, mp3 songs online, listen songs, MP3 songs, free online songs, wynk music, online latest songs, free music, popular songs, movie songs, listen to songs online, listen to free music, online music, free music online, latest movie songs, hindi songs, music albums, free music, online music, english songs, hollywood songs, playlists music, music online, hindi songs, play songs, online songs",
+    "Wynk UI clone, Download songs, music, mp3 songs online, listen songs, MP3 songs, free online songs, wynk music, online latest songs, free music, popular songs, movie songs, listen to songs online, listen to free music, online music, free music online, latest movie songs, hindi songs, music albums, free music, online music, english songs, hollywood songs, playlists music, music online, hindi songs, play songs, online songs",
 };
